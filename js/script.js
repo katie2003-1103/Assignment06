c0fa59f8e95bd72e54cee02b12b3cc28b6f81c9d
Kaitlyn Gonzalez <kaitlyn.l.gonzalez@gmail.com>
2025-11-05 15:16:58 -0800

    Updating

--- js/script.js
+++ js/script.js
@@ -23,23 +23,33 @@ let list = document.querySelector('empCount')
 
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
-    // PREVENT FORM SUBMISSION
+// PREVENT FORM SUBMISSION
     e.preventDefault()
-    // GET THE VALUES FROM THE TEXT BOXES
+// GET THE VALUES FROM THE TEXT BOXES
     let newItem = document.querySelector('item').value 
-    // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
+// INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
+    let row = thetable.insertRow()
+    let cellid = row.insertCell('id')
+    let cellname = row.insertCell('name')
+    let cellextension = row.insertCell('extension')
+    let cellemail = row.insertCell('email')
+    let celldepartment = row.insertCell('department')
+    let celldelete = row.insertCell('department')
+// INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
+    cellid.appendChild(document.createTextNode('id'))
+    cellname.appendChild(document.createTextNode('name'))
+    cellextension.appendChild(document.createTextNode('extension'))
+    cellemail.appendChild(document.createTextNode('email'))
+    celldepartment.appendChild(document.createTextNode('department'))
+// APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
 
-    // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
-
-    // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
-
-    // CREATE DELETE BUTTON 
+// CREATE DELETE BUTTON 
     let deleteBtn = document.createElement('button')
-    // ADD BOOTSTRAP CLASSES TO STYLIZE BUTTON
+// ADD BOOTSTRAP CLASSES TO STYLIZE BUTTON
     deleteBtn.className 'btn btn-danger btn-sm float-end'
-    // CREATE TEXT NODE FOR DELETE BUTTON AND SET TO 'X'
+// CREATE TEXT NODE FOR DELETE BUTTON AND SET TO 'X'
     let textDelete document.createTextNode('X')
-    //APPEND TEXT NODE TO DELETE BUTTON
+//APPEND TEXT NODE TO DELETE BUTTON
     deleteBtn.appendChild(textDelete)
     li.appendChild(deleteBtn) 
     
